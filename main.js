fe379fd9955db7e9373fdae4d95c3c16c890cef7
Vlada992 <vladanng@gmail.com>
2019-04-08 01:08:05 +0200

    filter 90% completed, css improved

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-var usersArray = [];
-var selectedIndex = -1;
-
-console.log('It is:', localStorage.studentsRecord)
+var usersArray = [],
+    selectedIndex = -1;
+var cardCodes, city, firstName, filterDOM;
+var cardCategory = [],
+    cardPercentage = [],
+    cardExpiration;
 
 function init() {
     document.getElementById("tablerows-0").innerHTML = "";
@@ -14,17 +16,18 @@ function init() {
 };
 
 function onRegisterPressed() {
-    var cardNumbArr = [], cardNumber, cardOptionId = [];
+    var cardNumbArr = [],
+        cardNumber, cardOptionId = [];
     var cardExpiration, dateFormatted;
-     cardOptionId = ['category-id', 'accumulation-id', 'percentage-id'];
-     cardExpiration = document.getElementById('expiration-id').value;
-     dateFormatted = moment(cardExpiration).format("DDMMYY"); 
+    cardOptionId = ['category-id', 'accumulation-id', 'percentage-id'];
+    cardExpiration = document.getElementById('expiration-id').value;
+    dateFormatted = moment(cardExpiration).format("DDMMYY");
 
     for (let n = 0; n < cardOptionId.length; n++) {
         cardNumbArr.push(document.getElementById(cardOptionId[n]).value);
     }
     cardNumbArr.push(dateFormatted)
-    cardNumber = Number(cardNumbArr.join(''))
+    cardNumber = Number(cardNumbArr.join(''));
 
     var firstName = document.getElementById("firstname-id").value;
     var city = document.getElementById("city-id").value;
@@ -47,29 +50,45 @@ function onRegisterPressed() {
 };
 
 
-function onSearchPressed() {
+function onSearchPressed(calledFrom) {
     var searchQuery = document.getElementById('search-querry').value;
-    var searchSpan = document.getElementById('search-span-id');
+    var arrayFromCode = [];
+    //var searchSpan = document.getElementById('search-span-id');
     if (localStorage.studentsRecord) {
         usersArray = JSON.parse(localStorage.studentsRecord);
 
-        const firstName = usersArray.map(u => u.firstname);
-        const city = usersArray.map(u => u.city);
-        const cardCodes = usersArray.map(u => u.cardCode);
-  
+        firstName = usersArray.map(u => u.firstname);
+        city = usersArray.map(u => u.city);
+        cardCodes = usersArray.map(u => u.cardCode);
+
+
+
         for (let i = 0; i < usersArray.length; i++) {
             document.getElementById('tablerows-0').children[i].classList.remove('searched-box')
-
             if (searchQuery == usersArray[i].firstname || searchQuery == usersArray[i].city || searchQuery == usersArray[i].cardCode) {
-                document.getElementById('search-btn').href = '#tablerows-0'; 
+                document.getElementById('search-btn').href = '#tablerows-0';
                 document.getElementById('tablerows-0').children[i].classList.add('searched-box')
-                //searchSpan.classList.remove('show-search-txt')
-                //searchSpan.classList.add('hide-search-txt')
-            } else if( (firstName.includes(searchQuery) || city.includes(searchQuery) || cardCodes.includes(searchQuery)) == false){
-                //searchSpan.classList.remove('hide-search-txt')
-                //searchSpan.classList.add('show-search-txt')
+
+                if (calledFrom == 'filter') {
+                    filterDOM = document.getElementById('filter-searched-code').value;
+                    console.log(filterDOM)
+
+                    cardPercentage.push((String(cardCodes[i])[2] + String(cardCodes[i])[3]).split(' ').join()) //percentage PART
+                    cardCategory.push(String(cardCodes[i])[0]); //card category PART
+                    cardExpiration.push(String(cardCodes[i].substr(4)))
+                    //--------------------------------------------------------------
+
+                    if (cardPercentage.includes(String(filterDOM)) || cardCategory.includes(filterDOM) || cardExpiration.includes(filterDOM)) {
+                        console.log('works')
+                        document.getElementById('tablerows-0').children[i].style.border = '5px solid blue'
+                    }
+                    //--------------------------------------------------------------
+                } //if called from filter buttons
+
+                //======================================================================================================================//
+            } else if ((firstName.includes(searchQuery) || city.includes(searchQuery) || cardCodes.includes(searchQuery)) == false) {
                 alert('Search term doesn\'t exist!')
-                return
+                return;
             }
         }
     }
@@ -77,33 +96,26 @@ function onSearchPressed() {
 
 
 
-function sortUsers(sortArg){
+function sortUsers(sortArg) {
     if (localStorage.studentsRecord) {
         var userProp = document.getElementById('sort-opt-ascending').value;
-        var userPropDesc = document.getElementById('sort-opt-descending').value
-    usersArray = JSON.parse(localStorage.studentsRecord);
+        var userPropDesc = document.getElementById('sort-opt-descending').value;
+        usersArray = JSON.parse(localStorage.studentsRecord);
 
-    if(sortArg == 'ascending'){
-        usersArray.sort(function(a, b){
-            return a[userProp] === b[userProp] ? 0 : a[userProp] < b[userProp] ? -1 : 1;
-        })    
-    } else {
-        usersArray.sort((a,b)=> {
-            return a[userPropDesc] === b[userPropDesc] ? 0 : a[userPropDesc] > b[userPropDesc] ? -1 : 1;
-        })
+        if (sortArg == 'ascending') {
+            usersArray.sort((a, b) => {
+                return a[userProp] === b[userProp] ? 0 : a[userProp] < b[userProp] ? -1 : 1; //ternary inside ternary.
+            })
+        } else {
+            usersArray.sort((a, b) => {
+                return a[userPropDesc] === b[userPropDesc] ? 0 : a[userPropDesc] > b[userPropDesc] ? -1 : 1;
+            })
+        }
+        localStorage.studentsRecord = JSON.stringify(usersArray); //send back to localStorage
+        init();
     }
-    
-    console.log(usersArray)
-    localStorage.studentsRecord = JSON.stringify(usersArray); //send back to localStorage
-    init();
-    }
-    //znaci sortiraj i skupinu sa istim imenima/prezimenima i slicno.
-}
+};
 
-function filterCardCode(){
-    console.log('test');
-    console.log(document.getElementById('category-id').value)
-}
 
 function prepareTableCell(index, firstName, city, cardCode) {
     var table = document.getElementById("tablerows-0");
@@ -116,9 +128,6 @@ function prepareTableCell(index, firstName, city, cardCode) {
     cityCell.innerHTML = city;
     cardCell.innerHTML = cardCode;
     actionCell.innerHTML = '<a class="btn btn-primary" href="#form-cont-id" onclick="onEditPressed(' + index + ')">Edit</a><br/><a class="btn btn-primary btn-prim-del" onclick="deleteTableRow(' + index + ')">Delete</a>';
-   //-----------------------------------------
-   //-----------------------------------------
-   
 };
 
 
@@ -139,14 +148,14 @@ function onClarPressed() {
 };
 
 
-function onEditPressed(index) { 
+function onEditPressed(index) {
     selectedIndex = index;
     var cardInfo = usersArray[index];
     var submitDom = document.getElementById("submit");
     document.getElementById("firstname-id").value = cardInfo.firstname;
     document.getElementById("city-id").value = cardInfo.city;
     submitDom.value = "Update";
-    submitDom.style.border= '2px solid darkred';
+    submitDom.style.border = '2px solid darkred';
     submitDom.style.boxShadow = '5px 5px 5px darkred';
     document.querySelector('.form-cont').style.backgroundColor = '#c4c4c4';
 };
